4d804241c36939ee962f04c1635aed3b62742c17
曾令燊 <dvsseed@users.noreply.github.com>
2017-10-09 17:33:29 +0800

    Create microbit_helper.sh

---
+++ scripts/macos/microbit_helper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Modify this file for your serial port designator.
+# Move this file to the directory that contains microbit_helper.py
+# and execute it by typing "./microbit_helper.sh"
+# Make sure that this file is executable as well as microbit_helper.py
+
+python microbit_helper.py /dev/cu.usbmodem1422     (or /dev/cu.usbmodem1412)
