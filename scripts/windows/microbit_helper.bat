d72369f3142d6794aaed52083d553b69c19bc596
曾令燊 <dvsseed@users.noreply.github.com>
2017-10-09 17:31:45 +0800

    Create microbit_helper.bat

---
+++ scripts/windows/microbit_helper.bat
@@ -0,0 +1,8 @@
+REM Modify this file for your serial port designator and for the location of python.
+REM Move this file to the directory that contains microbit_helpler.py
+REM and execute it by opening the file explorer and double clicking.
+REM You can also create a desktop shortcut if you like.
+REM Right-click on this file in the Windows File Explorer and select shortcut to create the shortcut.
+REM Make sure that this file is executable as well as microbit_helpler.py
+
+C:\Python27\python microbit_helpler.py com14
