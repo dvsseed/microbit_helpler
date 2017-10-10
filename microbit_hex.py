5ab30463ab018a0e6febd361f1c716fe6ac52356
曾令燊 <dvsseed@users.noreply.github.com>
2017-10-10 01:16:31 -0500

    the MicroPython script for microbit

--- microbit_hex.py
+++ microbit_hex.py
@@ -3,7 +3,7 @@ from microbit import *
 
 import music
 
-REFRESH = 500
+REFRESH = 150
 
 def get_sensor_data():
     # Gestures/Accelerometer
@@ -29,52 +29,56 @@ def str2bool(s):
 def read_uart():
     uart.init(115200, bits=8, parity=None, stop=1)
     try:
+        # bytestring = uart.readline()
+        # icon = str(bytestring, 'utf-8')
         bytestring = uart.readline()
-        icon = str(bytestring, 'utf-8')
-        if icon.find('OFF.') != -1:
+        data_list = str(bytestring, 'utf-8').split(':')
+        off_0, offreset_1, offmusic_2, led_3, pin_4, music_5 = data_list
+        if off_0 != 'none':
             # Set the brightness of all LEDs to 0 (off).
             display.clear()
-        elif icon.find('OFFRESET.') != -1:
+        if offreset_1 != 'none':
             # Restart the board.
             reset()
-        elif icon.find('OFFMUSIC.') != -1:
+        if offmusic_2 != 'none':
             # Stops all music playback on a given pin0.
             music.stop()
-        elif icon.find('Image.') != -1:
-            # LED
-            # Display, x, y = 0~4, value = 0~9
-            # display.set_pixel(x, y, value)
+        if led_3 != 'none':
+            if led_3.find('Image.') != -1:
+                # LED
+                # Display, x, y = 0~4, value = 0~9
+                # display.set_pixel(x, y, value)
 
-            # Image
-            # image = Image("90009:"
-            #            "09090:"
-            #            "00900:"
-            #            "09090:"
-            #            "90009")
-            display.show(getattr(Image, icon.split('.')[1]))
-        elif icon.find('P0.') != -1 or icon.find('P1.') != -1 or icon.find('P2.') != -1:
+                # Image
+                # image = Image("90009:"
+                #            "09090:"
+                #            "00900:"
+                #            "09090:"
+                #            "90009")
+                display.show(getattr(Image, led_3.split('.')[1]))
+            else:
+                display.show(led_3.split('.')[1])                
+        if pin_4 != 'none':
             # Input/Output Pins
             # if pin0.is_touched():
-            if icon[1:2] == '0':
-                pin0.write_digital(int(icon.split('.')[1]))
-            elif icon[1:2] == '1':
-                pin1.write_digital(int(icon.split('.')[1]))
-            elif icon[1:2] == '2':
-                pin2.write_digital(int(icon.split('.')[1]))
-        elif icon.find('music.') != -1:
-            music.play(getattr(music, icon.split('.')[1]), wait=False, loop=str2bool(icon.split('.')[2]))
-        else:
-            display.show(icon)
+            if pin_4[1:2] == '0':
+                pin0.write_digital(int(pin_4.split('.')[1]))
+            elif pin_4[1:2] == '1':
+                pin1.write_digital(int(pin_4.split('.')[1]))
+            elif pin_4[1:2] == '2':
+                pin2.write_digital(int(pin_4.split('.')[1]))
+        if music_5 != 'none':
+            music.play(getattr(music, music_5.split('.')[1]), wait=False, loop=str2bool(music_5.split('.')[2]))
     except:
         pass
 
 def run():
     while True:
         # Do stuff
-        sleep(REFRESH)
         get_sensor_data()
         sleep(REFRESH)
         read_uart()
+        sleep(REFRESH)
 
 display.show('M')
 
