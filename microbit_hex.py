b0d12fc612add687f661e14ab6c6efc9d7a8ec75
曾令燊 <dvsseed@users.noreply.github.com>
2017-10-11 19:32:32 -0500

    Add files via upload

--- microbit_hex.py
+++ microbit_hex.py
@@ -3,7 +3,7 @@ from microbit import *
 
 import music
 
-REFRESH = 150
+REFRESH = 100
 
 def get_sensor_data():
     # Gestures/Accelerometer
@@ -30,9 +30,8 @@ def read_uart():
     uart.init(115200, bits=8, parity=None, stop=1)
     try:
         # bytestring = uart.readline()
-        # icon = str(bytestring, 'utf-8')
-        bytestring = uart.readline()
-        data_list = str(bytestring, 'utf-8').split(':')
+        # data = str(bytestring, 'utf-8')
+        data_list = str(uart.readline(), 'utf-8').split(':')
         off_0, offreset_1, offmusic_2, led_3, pin_4, music_5 = data_list
         if off_0 != 'none':
             # Set the brightness of all LEDs to 0 (off).
@@ -44,31 +43,35 @@ def read_uart():
             # Stops all music playback on a given pin0.
             music.stop()
         if led_3 != 'none':
+            ledv = led_3.split('.')[1]
             if led_3.find('Image.') != -1:
                 # LED
                 # Display, x, y = 0~4, value = 0~9
                 # display.set_pixel(x, y, value)
-
                 # Image
                 # image = Image("90009:"
                 #            "09090:"
                 #            "00900:"
                 #            "09090:"
                 #            "90009")
-                display.show(getattr(Image, led_3.split('.')[1]))
+                display.show(getattr(Image, ledv))
             else:
-                display.show(led_3.split('.')[1])                
+                display.show(ledv)                
         if pin_4 != 'none':
             # Input/Output Pins
             # if pin0.is_touched():
-            if pin_4[1:2] == '0':
-                pin0.write_digital(int(pin_4.split('.')[1]))
-            elif pin_4[1:2] == '1':
-                pin1.write_digital(int(pin_4.split('.')[1]))
-            elif pin_4[1:2] == '2':
-                pin2.write_digital(int(pin_4.split('.')[1]))
+            pink = pin_4[1:2]
+            pinv = int(pin_4.split('.')[1])
+            if pink == '0':
+                pin0.write_digital(pinv)
+            elif pink == '1':
+                pin1.write_digital(pinv)
+            elif pink == '2':
+                pin2.write_digital(pinv)
         if music_5 != 'none':
-            music.play(getattr(music, music_5.split('.')[1]), wait=False, loop=str2bool(music_5.split('.')[2]))
+            musicv1 = music_5.split('.')[1]
+            musicv2 = str2bool(music_5.split('.')[2])
+            music.play(getattr(music, musicv1, wait=False, loop=musicv2))
     except:
         pass
 
@@ -76,7 +79,6 @@ def run():
     while True:
         # Do stuff
         get_sensor_data()
-        sleep(REFRESH)
         read_uart()
         sleep(REFRESH)
 
